212ed524d777436ff0626e1d85bb9a320cf6125f
Mark Allred <markallred@Marks-MacBook-Pro-2.local>
2020-03-28 16:18:32 -0600

    Initial implementation

--- parse.py
+++ parse.py
@@ -1,1 +1,55 @@
+from collections import OrderedDict
 from PyPDF2 import PdfFileReader
+
+def _getFields(obj, tree=None, retval=None, fileobj=None):
+    """
+    Extracts field data if this PDF contains interactive form fields.
+    The *tree* and *retval* parameters are for recursive use.
+
+    :param fileobj: A file object (usually a text file) to write
+        a report to on all interactive form fields found.
+    :return: A dictionary where each key is a field name, and each
+        value is a :class:`Field<PyPDF2.generic.Field>` object. By
+        default, the mapping name is used for keys.
+    :rtype: dict, or ``None`` if form data could not be located.
+    """
+    fieldAttributes = {'/FT': 'Field Type', '/Parent': 'Parent', '/T': 'Field Name', '/TU': 'Alternate Field Name',
+                       '/TM': 'Mapping Name', '/Ff': 'Field Flags', '/V': 'Value', '/DV': 'Default Value'}
+    if retval is None:
+        retval = OrderedDict()
+        catalog = obj.trailer["/Root"]
+        # get the AcroForm tree
+        if "/AcroForm" in catalog:
+            tree = catalog["/AcroForm"]
+        else:
+            return None
+    if tree is None:
+        return retval
+
+    obj._checkKids(tree, retval, fileobj)
+    for attr in fieldAttributes:
+        if attr in tree:
+            # Tree is a field
+            obj._buildField(tree, retval, fileobj, fieldAttributes)
+            break
+
+    if "/Fields" in tree:
+        fields = tree["/Fields"]
+        for f in fields:
+            field = f.getObject()
+            obj._buildField(field, retval, fileobj, fieldAttributes)
+
+    return retval
+
+def get_form_fields(infile):
+    infile = PdfFileReader(open(infile, 'rb'))
+    fields = _getFields(infile)
+    return OrderedDict((k, v.get('/V', '')) for k, v in fields.items())
+
+def main():
+    from pprint import pprint
+    infilePath = "/Users/markallred/Desktop/SBA_test_apps/test1.pdf"
+    pprint(get_form_fields(infilePath))
+
+if __name__ == '__main__':
+    main()
